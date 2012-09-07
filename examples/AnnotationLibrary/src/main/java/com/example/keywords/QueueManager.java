fa9b59a2430431d3c492e98f63d97d854dad2317
Kevin Ormbrek <kormbrek@gmail.com>
2012-09-06 20:48:34 -0500

    simplify example

--- examples/AnnotationLibrary/src/main/java/com/example/keywords/QueueManager.java
+++ examples/AnnotationLibrary/src/main/java/com/example/keywords/QueueManager.java
@@ -1,5 +1,8 @@
 package com.example.keywords;
 
+import static org.apache.commons.lang.StringEscapeUtils.escapeHtml;
+
+import java.util.Iterator;
 import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -7,25 +10,50 @@ import org.robotframework.javalib.annotation.ArgumentNames;
 import org.robotframework.javalib.annotation.RobotKeyword;
 import org.robotframework.javalib.annotation.RobotKeywords;
 
-// this class owns the queue
 @RobotKeywords
 public class QueueManager
 {
-    private static Queue<Object> queue = new LinkedBlockingQueue<Object>();
-    
-    /*
-     * This singleton-like design means that you could only serve one instance of this test library per virtual
-     * machine. A more flexible approach would be to maintain a Map containing instances of this class keyed by port.
-     * The constructor could insert the instance into the Map. To determine the port, get the request with
-     * {@link org.robotframework.remoteserver.servlet.RemoteServerServlet#getRequest()}.
-     */
-    static Queue<Object> getQueue() {
-        return queue;
-    }
-    
+    private Queue<Object> queue = new LinkedBlockingQueue<Object>();
+
     @RobotKeyword("Clears the queue.")
     @ArgumentNames({})
     public void clearQueue() {
         queue.clear();
+    }
+
+    @RobotKeyword("Adds the given `element` to tail of the queue.")
+    @ArgumentNames({"element"})
+    public void addToQueue(Object element) {
+        queue.add(element);
+    }
+
+    @RobotKeyword("Removes the element at the head of the queue.")
+    @ArgumentNames({})
+    public Object removeFromQueue() {
+        return queue.remove();
+    }
+
+    @RobotKeyword("Retrieves the queue.")
+    @ArgumentNames({})
+    public Queue<Object> getQueue() {
+        return queue;
+    }
+
+    @RobotKeyword("Logs the elements in the queue.")
+    @ArgumentNames({})
+    public void logQueue() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("*HTML* <table border='1'><tr><th>Value</th><th>Type</th></tr>");
+        Iterator<Object> iter = queue.iterator();
+        while (iter.hasNext()) {
+            Object element = iter.next();
+            String value = escapeHtml(element.toString());
+            sb.append("<tr>");
+            sb.append("<td>" + value + "</td>");
+            sb.append("<td>" + element.getClass().getName() + "</td>");
+            sb.append("</tr>");
+        }
+        sb.append("</table>");
+        System.out.print(sb.toString());
     }
 }
