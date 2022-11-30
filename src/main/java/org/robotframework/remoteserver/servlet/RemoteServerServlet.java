2754d005ded9e7ecd24ea78e427d014f6dbe4a2d
vhup <38454387+vhup@users.noreply.github.com>
2022-11-30 08:57:47 +0100

    cleanup

--- src/main/java/org/robotframework/remoteserver/servlet/RemoteServerServlet.java
+++ src/main/java/org/robotframework/remoteserver/servlet/RemoteServerServlet.java
@@ -21,12 +21,6 @@ import java.util.Map;
 import java.util.TreeMap;
 import java.util.concurrent.ConcurrentHashMap;
 
-/*
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-*/
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
